eea0439df2e08f4a9e02723bd0e8267bdeeebd50
GiovannaK <giovannakelli@gmail.com>
2023-05-17 21:23:15 -0300

    chore: setup prisma

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaService } from './database/prisma/prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prismaService: PrismaService,
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello() {
+    const users = await this.prismaService.user.findMany();
+    return users;
   }
 }
