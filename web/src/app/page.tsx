ad19880666ff73c5589296a4cfc76c75093e484e
GiovannaK <giovannakelli@gmail.com>
2023-05-17 22:03:51 -0300

    update web

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div className="text-2xl font-bold uppercase">Oi</div>
+  return (
+    <div className="h-screen bg-zinc-900">
+      <h1 className="text-zinc-50">Hey</h1>
+    </div>
+  )
 }
